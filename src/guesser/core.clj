2f2afa3e2adba7730950a3fde99ce59c84169687
Justin Reese <justin.x.reese@gmail.com>
2013-01-21 20:47:37 -0500

    Multiple arity on parameters <- WUT

--- src/guesser/core.clj
+++ src/guesser/core.clj
@@ -2,5 +2,10 @@
 
 (defn number-guess
   "Compare my guess to a random number"
+  (
   [guess random-fn]
   (if (== guess (random-fn)) "yay" "nay!"))
+  (
+  [guess s random-fn]
+  (if (== guess (random-fn)) (str "yay " s) "nay!"))
+  )
--- test/guesser/core_test.clj
+++ test/guesser/core_test.clj
@@ -7,3 +7,9 @@
     (is (= (number-guess 2 (fn [] 2)) "yay"))
     (is (= (number-guess 3 (fn [] 2)) "nay!"))
     ))
+
+(deftest number-guess-test-with-string
+  (testing "If the number is the same, say 'yay' and my string"
+    (is (= (number-guess 2 "horse" (fn [] 2)) "yay horse"))
+    (is (= (number-guess 3 "horse" (fn [] 2)) "nay!"))
+    ))

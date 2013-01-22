d6440a35586b538536430921395dd04835e99ecd
Justin Reese <justin.x.reese@gmail.com>
2013-01-22 11:44:55 -0500

    Refactors some stuff at @benizi's suggestion

--- src/guesser/core.clj
+++ src/guesser/core.clj
@@ -4,8 +4,8 @@
   "Compare my guess to a random number"
   (
   [guess random-fn]
-  (if (== guess (random-fn)) "yay" "nay!"))
+  (if (= guess (random-fn)) "yay" "nay!"))
   (
   [guess s random-fn]
-  (if (== guess (random-fn)) (str "yay " s) "nay!"))
+  (if (= guess (random-fn)) (str "yay " s) "nay!"))
   )
--- test/guesser/core_test.clj
+++ test/guesser/core_test.clj
@@ -4,12 +4,12 @@
 
 (deftest number-guess-test
   (testing "If the number is the same, say 'yay'"
-    (is (= (number-guess 2 (fn [] 2)) "yay"))
-    (is (= (number-guess 3 (fn [] 2)) "nay!"))
+    (is (= (number-guess 2 (constantly 2)) "yay"))
+    (is (= (number-guess 3 (constantly 2)) "nay!"))
     ))
 
 (deftest number-guess-test-with-string
   (testing "If the number is the same, say 'yay' and my string"
-    (is (= (number-guess 2 "horse" (fn [] 2)) "yay horse"))
-    (is (= (number-guess 3 "horse" (fn [] 2)) "nay!"))
+    (is (= (number-guess 2 "horse" (constantly 2)) "yay horse"))
+    (is (= (number-guess 3 "horse" (constantly 2)) "nay!"))
     ))
